85b87865e5ec65d24dd76fb8b733c6ffe5fdbd16
Valentina <valentina.rachiele@gmail.com>
2014-10-05 12:38:03 +0200

    Generic fixes
    Fixed overall style and presentation

--- README.md
+++ README.md
@@ -5,16 +5,4 @@ synsemic-writing-cooker
 
 The aim of <em>Synsemic writing cooker</em> is to generate random exercises about synsemic writing. Basically, the system creates a list of different variables randomly generated – so each exercise is unique. 
 
-To create your own exercises, create a TXT file for each variable. The TXT file must have a variable on each line. For example, your file <strong>color.txt</strong> will be structured like so:
-
-<pre><code>yellow<br>
-red<br>
-blue<br>
-...<br>
-</code></pre>
-
-Then clone one of the PHP files (modifying its name, just to be sure there's a correspondance between TXT and PHP files) and change the line 3. For example, if your TXT file is named <strong>color.txt</strong>, your PHP file <strong>color.php</strong> will have the following code on line #3:
-
-<code>$settings['text_from_file'] = 'color.txt';</code>
-
-Be sure to check the <strong>index.php</strong> file too, because it controls the overall functioning.
+To create your own exercises, just modify the TXT files, putting a variable on each line. If you want to change the TXT files name, remember to change it in the index.php file too.
--- index.php
+++ index.php
@@ -1,125 +1,95 @@
 <!doctype html>
-<html>
+<html lang="en">
 <head>
-<title>Synsemic writing cooker</title>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Synsemic writing cooker">
 
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.4.2/pure-min.css">
-<link rel="stylesheet" href="css/style.css">
-<link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <title>Synsemic writing cooker</title>
+
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure.css">
+
+
+
+<!--[if lte IE 8]>
+  
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive-old-ie.css">
+  
+<![endif]-->
+<!--[if gt IE 8]><!-->
+  
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/grids-responsive.css">
+  
+<!--<![endif]-->
+  
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="css/layouts/blog-old-ie.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="css/style.css">
+    <!--<![endif]-->
 
 </head>
 <body>
-		
-	<div class="pure-g-r">
 
-		<div class="pure-u-6-24"></div>
-        <div class="pure-u-1-2">
-            <div class="l-box"> <h1>Synsemic writing cooker</h1> </div>
-        </div>
-        <div class="pure-u-6-24"></div>
+<div id="layout" class="pure-g">
+    <div class="sidebar pure-u-1 pure-u-md-1-4">
+        <div class="header">
+            <h1 class="brand-title">Synsemic writing cooker</h1>
 
-		<div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>tema</strong></p> </div>
+            <nav class="nav">
+                <ul class="nav-list">
+                    <li class="nav-item">
+                        <a class="pure-button" href="https://github.com/valentinarachiele/synsemic-writing-cooker">Github repository</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'tema.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-        <div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>compito</strong></p> </div>
-        </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'compito.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-        <div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>contesto</strong></p> </div>
-        </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'contesto.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-		<div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>formato</strong></p> </div>
-        </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'formato.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-        <div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>colori</strong></p> </div>
-        </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'colori.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-		<div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>tecnica</strong></p> </div>
-        </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'tecnica.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-        <div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>tempo</strong></p> </div>
-        </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'tempo.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-        <div class="pure-u-6-24"></div>
-        <div class="pure-u-4-24">
-            <div class="l-box right"> <p><strong>limiti</strong></p> </div>
-        </div>
-        <div class="pure-u-8-24">
-        	<div class="l-box">
-            	<p><?php $_GET['type'] = 0; include 'limiti.php'; ?></p>
-        	</div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-        <div class="pure-u-1">&nbsp;</div>
-
-		<div class="pure-u-6-24"></div>
-        <div class="pure-u-1-2">
-            <div class="center"> <input class="pure-button pure-button-primary" type="button" value="Go!" onclick="location='index.php'" /> </div>
-        </div>
-        <div class="pure-u-6-24"></div>
-
-        <div class="pure-u-6-24 footer"></div>
-        <div class="pure-u-1-2">
-            <div class="center">&copy;Valentina Rachiele</div>
-        </div>
-        <div class="pure-u-6-24"></div>
     </div>
+
+    <div class="content pure-u-1 pure-u-md-3-4">
+        <div>
+			<div class="pure-g">
+			   	<div class="pure-u-1-3"> tema </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'tema.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+			   	<div class="pure-u-1-3"> compito </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'compito.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+			   	<div class="pure-u-1-3"> contesto </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'contesto.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+			   	<div class="pure-u-1-3"> formato </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'formato.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+			   	<div class="pure-u-1-3"> colori </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'colori.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+			   	<div class="pure-u-1-3"> tempo </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'tempo.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+			   	<div class="pure-u-1-3"> tecnica </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'tecnica.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+			   	<div class="pure-u-1-3"> limiti </div>
+			   	<div class="pure-u-1-3"> <?php $_GET['type'] = 0; include 'limiti.php'; ?></div>
+                <div class="pure-u-1-3"></div>
+                <div class="pure-u-1-3"></div>
+                <div class="pure-u-1-3">
+			   	<input class="pure-button pure-button-primary" type="button" value="Go!" onclick="location='index.php'" /></div>
+			</div>
+        </div>
+
+            <div class="footer">
+                <div class="pure-menu pure-menu-horizontal pure-menu-open">
+                    <ul>
+                        <li>&copy;2014 Valentina Rachiele</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 </body>
 </html>
